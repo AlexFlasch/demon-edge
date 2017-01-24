81a45331f5b7484d285c4d80e61261b5d93e5642
Flascher <alexanderflasch@gmail.com>
2017-01-23 22:49:49 -0600

    Fixes

--- src/Utils.js
+++ src/Utils.js
@@ -24,53 +24,53 @@ module.exports = {
 	},
 
 	// concatenates all the urlSegments into one http url with query parameters
-	generateRequestUrl(urlSegments, parameters) {
-		var requestUrl = this.daedalusUrl
-
-		var parameterNames = Object.keys(parameters);
-
-		requestUrl += '?';
-
-		// append parameters as such: &<param_name>=<param_value>
-		for (let i = 0; i < parameterNames.length; i++) {
-			if(i !== 0) {
-				requestUrl += '&';
-			}
-			requestUrl += `${parameterNames[i]}=${parameters[parameterNames[i]]}`;
-		}
-
-		return requestUrl;
-	},
+	// generateRequestUrl(urlSegments, parameters) {
+	// 	var requestUrl = this.daedalusUrl
+	//
+	// 	var parameterNames = Object.keys(parameters);
+	//
+	// 	requestUrl += '?';
+	//
+	// 	// append parameters as such: &<param_name>=<param_value>
+	// 	for (let i = 0; i < parameterNames.length; i++) {
+	// 		if(i !== 0) {
+	// 			requestUrl += '&';
+	// 		}
+	// 		requestUrl += `${parameterNames[i]}=${parameters[parameterNames[i]]}`;
+	// 	}
+	//
+	// 	return requestUrl;
+	// },
 
 	// sends a vanilla js xhr request, but wrapped in an ES6 promise
 	// in order to allow for contacting a separate api server, CORS has to be enabled
 	// return the promise object for the user to resolve when needed
 	sendXHR(url, params) {
-		const promise = new Promise((resolve, reject) => {
-			const xhr = new XMLHttpRequest();
+		// const promise = new Promise((resolve, reject) => {
+		// 	const xhr = new XMLHttpRequest();
+		//
+		// 	console.log(`http://${daedalusUrl}/${url}`);
+		//
+		// 	xhr.open('POST', `http://${daedalusUrl}/${url}`, true);
+		// 	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		//
+		// 	xhr.onload = function onload() {
+		// 		if (this.status >= 200 && this.status <= 300) {
+		// 			resolve(this.response);
+		// 		} else {
+		// 			reject(this.statusText);
+		// 		}
+		// 	};
+		//
+		// 	xhr.onerror = function onerror() {
+		// 		reject(this.statusText);
+		// 	};
+		//
+		// 	xhr.send(params);
+		// });
 
-			console.log(`http://${daedalusUrl}/${url}`);
+		return axios.post(`http://${this.daedalusUrl}/${url}`, params);
 
-			xhr.open('POST', `http://${daedalusUrl}/${url}`, true);
-			xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-			xhr.onload = function onload() {
-				if (this.status >= 200 && this.status <= 300) {
-					resolve(this.response);
-				} else {
-					reject(this.statusText);
-				}
-			};
-
-			xhr.onerror = function onerror() {
-				reject(this.statusText);
-			};
-
-			xhr.send(params);
-		});
-
-		return axios.post(`http://${this.daedalusUrl}/${url}`, )
-
-		return promise;
+		// return promise;
 	}
 };
